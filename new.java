86e593a7f0987a294bdb5c5b2a8e224906ec52a7
mavishk <95980450+mavishk@users.noreply.github.com>
2022-01-21 20:13:01 +0000

    Update new.java

--- new.java
+++ new.java
@@ -1,7 +1,7 @@
 class Example{
 public static void main(String args[]){
 
-System.out.println("Welcome to my first job on jenkins labs");
+System.out.println("Welcome to my first job on jenkins labsand git clone");
 
 }
 }
